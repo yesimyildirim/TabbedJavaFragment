6a3a982f4c50b486e63a270fd8383fa07b1e0ef9
yesimyildirim <yesimyildirim14@gmail.com>
2020-05-30 18:23:40 +0300

    TabbedFragment

---
+++ app/src/main/java/com/example/tabbedjavafragment/ui/main/SecondFragment.java
@@ -0,0 +1,54 @@
+package com.example.tabbedjavafragment.ui.main;
+
+import android.graphics.pdf.PdfDocument;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.tabbedjavafragment.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class SecondFragment extends Fragment {
+
+    PageViewModel pageViewModel;
+
+   public static SecondFragment newInstance(){
+       return new SecondFragment();
+   }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        pageViewModel = ViewModelProviders.of(requireActivity()).get(PageViewModel.class);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_second, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        final TextView textView = view.findViewById(R.id.textView);
+        pageViewModel.getName().observe(requireActivity(), new Observer<String>() {
+            @Override
+            public void onChanged(String s) {
+                textView.setText(s);
+            }
+        });
+    }
+}
